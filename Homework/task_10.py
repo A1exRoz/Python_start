414b9bbc8c19742d73a83f7948cc9b549de5a88a
alexrozanov <alexrozans@gmail.com>
2023-03-10 18:57:20 +0300

    add new var

--- Homework/task_10.py
+++ Homework/task_10.py
@@ -4,21 +4,32 @@
 чтобы все монетки были повернуты вверх одной и той же стороной. 
 Выведите минимальное количество монет, которые нужно перевернуть"""
 
-N = int(input('Сколько монет лежит на столе?: '))
-coin_eagle = 0
-coin_tails = 0
-if N == 1:
-    print('Нет смысла считать')
+# N = int(input('Сколько монет лежит на столе?: '))
+# coin_eagle = 0
+# coin_tails = 0
+# if N == 1:
+#     print('Нет смысла считать')
+# else:
+#     for i in range(N):
+#         coin = int(input('Какая следующая монета? Орел - 1, Решка - 0: '))
+#         if coin == 1:
+#             coin_eagle += 1
+#         else:
+#             coin_tails += 1
+#     if coin_eagle > coin_tails:
+#         print(f'Решек - {coin_tails} шт')
+#     elif coin_tails > coin_eagle:
+#         print(f'Орлов - {coin_eagle} шт')
+#     else:
+#         print('Без разницы, одинаковое кол-во монет')
+
+
+coin_eagle = int(input('Сколько монет лежит вверх орлом?: '))
+coin_tails = int(input('Сколько монет лежит вверх решкой?: '))
+n_coin = print(f'Общее число монет: {coin_eagle + coin_tails}')
+if coin_eagle > coin_tails:
+    print(f'Решек - {coin_tails} шт')
+elif coin_tails > coin_eagle:
+    print(f'Орлов - {coin_eagle} шт')
 else:
-    for i in range(N):
-        coin = int(input('Какая следующая монета? Орел - 1, Решка - 0: '))
-        if coin == 1:
-            coin_eagle += 1
-        else:
-            coin_tails += 1
-    if coin_eagle > coin_tails:
-        print(f'Решек - {coin_tails} шт')
-    elif coin_tails > coin_eagle:
-        print(f'Орлов - {coin_eagle} шт')
-    else:
-        print('Без разницы, одинаковое кол-во монет')
+    print('Без разницы, одинаковое кол-во монет')
